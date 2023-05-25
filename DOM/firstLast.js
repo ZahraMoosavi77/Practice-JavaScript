e4c73563f0efd896e7cfb65dd28e9b52d6057e1c
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-05-26 02:24:29 +0330

    changes

--- DOM/firstLast.js
+++ DOM/firstLast.js
@@ -1,13 +1,15 @@
 function changeTextLi(){
-    const list = document.querySelectorAll('ul');
-    // console.log(list)
+    // const list = document.querySelectorAll('ul');
+    const list = document.getElementsByTagName('ul');
+    console.log(list[0])
     // console.log(list[1].innerText[0]);
     list.forEach(ul=>{
         let li = ul.querySelectorAll('li');
         let length = li.length;
         li[0].innerText = 'first';
         li[length- 1].innerText = 'last';
-    
+        // lastElementChild
+        // firstElementChild
         // console.log(ul.innerText[0]);
         // ul.innerText[0] = "first" ;
         
