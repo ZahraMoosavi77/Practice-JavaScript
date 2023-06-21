42389fd2075c68123566f603d1b57092d567e10f
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-06-21 19:01:35 +0330

    active links

---
+++ portfolio/js/script.js
@@ -0,0 +1,33 @@
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+// scroll section
+window.onscroll=()=>{
+
+    sections.map(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if(top > offset && top < offset + height){
+            //active navbar links
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id+ ']').classList.add('active')
+
+            })
+        }
+    })
+
+
+
+
+
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky' , window.scrollY > 100)
+}
