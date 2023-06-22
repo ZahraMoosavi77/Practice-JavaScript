76becbe27eae1cda7bc0131fb345823808dfa7d6
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-06-22 19:37:39 +0330

    add brakpoints

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,3 +1,13 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick=() =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a')
@@ -26,8 +36,13 @@ window.onscroll=()=>{
 
 
 
-
+    //steaky headder
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky' , window.scrollY > 100)
+
+    //remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x')//* 
+    navbar.classList.remove('active');
+
 }
